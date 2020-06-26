a6217bf723bf2cc720bf4a98618a87087aee6c9f
ZanHaitao <99999966@outlook.com>
2020-06-26 15:58:40 +0800

    50.过渡-复用过渡

--- test-vue/src/App.vue
+++ test-vue/src/App.vue
@@ -1,13 +1,31 @@
 <template>
-  <base-demo></base-demo>
+  <div>
+    <button @click="show = !show">click</button>
+    <!-- <transition-demo>
+      <div v-if="show">hello world!</div>
+    </transition-demo>
+    <transition-demo>
+      <div v-if="show">hello eee!</div>
+    </transition-demo>-->
+
+    <transition-demo>
+      <div key="1" v-if="show">hello world!</div>
+      <div key="2" v-else>hello xiamen!</div>
+    </transition-demo>
+  </div>
 </template>
 
 <script>
-import BaseDemo from "./components/BaseDemo3";
+import TransitionDemo from "./components/TransitionDemo";
 
 export default {
   components: {
-    BaseDemo
+    TransitionDemo
+  },
+  data() {
+    return {
+      show: true
+    };
   }
 };
 </script>
---
+++ test-vue/src/components/TransitionDemo.vue
@@ -0,0 +1,32 @@
+
+
+<script>
+export default {
+  functional: true,
+  render(h, context) {
+    const { slots } = context;
+    return (
+      <transition name="demo" mode="out-in">
+        {slots().default}
+      </transition>
+    );
+  }
+};
+</script>
+
+<style>
+.demo-enter,
+.demo-leave-to {
+  opacity: 0;
+}
+
+.demo-enter-active,
+.demo-leave-active {
+  transition: all 0.5s;
+}
+
+.demo-enter-to,
+.demo-leave {
+  opacity: 1;
+}
+</style>
