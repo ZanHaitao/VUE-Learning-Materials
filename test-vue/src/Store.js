7f55f7bf61a438f73637043d8cd8fd76b71c45fb
ZanHaitao <99999966@outlook.com>
2020-06-29 13:53:01 +0800

    62.Vuex-Getter

--- test-vue/src/Store.js
+++ test-vue/src/Store.js
@@ -6,5 +6,9 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     studentList: []
+  },
+  getters: {
+    studentLength: state => state.studentList.length,
+    studentMinor: state => state.studentList.filter(item => item.age < 18)
   }
 })
--- test-vue/src/components/StudentList.vue
+++ test-vue/src/components/StudentList.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="student-list">
+    <div>学生总数：{{ studentLength }}</div>
+    <hr />
     <div>学生列表</div>
     <ul>
       <li v-for="student in studentList" :key="student.id">姓名：{{student.name}} 年龄：{{ student.age }}</li>
+    </ul>
+    <hr />
+    <div>未成年学生列表</div>
+    <ul>
+      <li v-for="student in studentMinor" :key="student.id">姓名：{{student.name}} 年龄：{{ student.age }}</li>
     </ul>
   </div>
 </template>
 
 <script>
-import { mapState } from "vuex";
+import { mapState, mapGetters } from "vuex";
 
 export default {
   computed: {
-    ...mapState(["studentList"])
+    ...mapState(["studentList"]),
+    ...mapGetters(["studentMinor", "studentLength"])
   }
 };
 </script>
